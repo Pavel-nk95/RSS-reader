af8d2df8246d4ae2fac5e9e5c6ef0a0cacb63f32
Pavel Nk <pav.n95@gmail.com>
2022-05-05 19:58:20 +0300

    test #5

--- src/update.js
+++ src/update.js
@@ -5,29 +5,26 @@ import parser from './parser.js';
 import createUrl from './createUrl.js';
 
 const update = (state, delay) => {
-  function request() {
-    Promise.all(
-      state.links.map((url) => (
-        Promise.resolve(url)
-          .then((link) => axios.get(createUrl(link)))
-          .then((response) => parser(response))
-          .then((data) => {
-            const posts = data.posts.map((item) => {
-              state.postID += 1;
-              item.id = state.postID;
-              item.feedId = state.feedID;
-              return item;
-            });
-            const difference = _.differenceBy(posts, state.posts, 'title');
-            state.posts.push(...difference);
-            state.posts = _.sortBy(state.posts, ['feedId', 'id']);
-          })
-      )),
-    ).finally(() => {
-      setTimeout(request, delay);
-    });
-  }
-  request();
+  Promise.all(
+    state.links.map((url) => (
+      Promise.resolve(url)
+        .then((link) => axios.get(createUrl(link)))
+        .then((response) => parser(response))
+        .then((data) => {
+          const posts = data.posts.map((item) => {
+            state.postID += 1;
+            item.id = state.postID;
+            item.feedId = state.feedID;
+            return item;
+          });
+          const difference = _.differenceBy(posts, state.posts, 'title');
+          state.posts.push(...difference);
+          state.posts = _.sortBy(state.posts, ['feedId', 'id']);
+        })
+    )),
+  ).finally(() => {
+    setTimeout(() => update(state, delay), delay);
+  });
 };
 
 export default update;
